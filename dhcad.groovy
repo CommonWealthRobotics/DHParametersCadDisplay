4cfb6312e8217bd176f2d0d9311ee65e7e2e4747
Kevin Harrington <mad.hephaestus@gmail.com>
2019-05-19 15:34:25 -0400

    generating all link frames in their approprate link

--- dhcad.groovy
+++ dhcad.groovy
@@ -17,12 +17,13 @@ return new ICadGenerator(){
 		return incoming.transformed(move)
 
 	}
-
-	@Override
-	public ArrayList<CSG> generateCad(DHParameterKinematics d, int linkIndex) {
-		
-		ArrayList<CSG> allCad=new ArrayList<>();
+	
+	ArrayList<CSG> linkLimitParts(DHParameterKinematics d, int linkIndex){
 		ArrayList<DHLink> dhLinks = d.getChain().getLinks()
+		if(linkIndex >= dhLinks.size()){
+			return []
+		}
+		ArrayList<CSG> allCad=new ArrayList<>();
 		DHLink dh = dhLinks.get(linkIndex)
 		// Hardware to engineering units configuration
 		LinkConfiguration conf = d.getLinkConfiguration(linkIndex);
@@ -31,20 +32,7 @@ return new ICadGenerator(){
 		// Transform used by the UI to render the location of the object
 		Affine manipulator = dh.getListener();
 		Affine lastLinkFrame
-		def massKg = conf.getMassKg()
-		def centerOfMass = TransformFactory.nrToCSG(conf.getCenterOfMassFromCentroid() )
-		def CMvis = new Sphere(500*massKg).toCSG()
-					.transformed(centerOfMass)
-		
-		if(linkIndex==0)
-			lastLinkFrame=d.getRootListener()
-		else
-			lastLinkFrame=dhLinks.get(linkIndex-1).getListener();
-		
-		def rVal = new Cube(dh.getR()>0?dh.getR():5,1,1).toCSG()
-					.toXMax()
-					.toZMax()
-		rVal.setColor(javafx.scene.paint.Color.RED)
+
 		CSG profile = new Cube(1,// x dimention
 					20,// y dimention
 				
@@ -75,27 +63,6 @@ return new ICadGenerator(){
 		.movez(-0.5)
 		theta.setColor(javafx.scene.paint.Color.AQUA)
 
-		CSG alpha;
-		double alphaVal = Math.toDegrees(dh.getAlpha())
-		if(Math.abs(alphaVal)>10){
-			alpha= CSG.unionAll(
-			Extrude.revolve(profile,
-						0, // rotation center radius, if 0 it is a circle, larger is a donut. Note it can be negative too
-						Math.abs(alphaVal),// degrees through wich it should sweep
-						(int)10)//number of sweep increments
-			)
-			//.rotz(alphaVal<0?-alphaVal:0)
-		}else{
-			alpha = profile
-		}
-		alpha= alpha.roty(90)
-		if(alphaVal>0){
-			alpha= alpha.rotx(alphaVal)	
-		}
-		alpha= alpha
-			.rotx(-90)
-			.movex(-dh.getR())
-		alpha.setColor(javafx.scene.paint.Color.YELLOW)
 
 		def dpart = new Cube(1,1,dh.getD()>0?dh.getD():1).toCSG()
 					.toZMin()
@@ -146,21 +113,108 @@ return new ICadGenerator(){
 		def lastFrameParts = [
 		theta,
 		dpart,upperLim,lowerLim,zeroLim,Range]
+
+		return lastFrameParts;
+		
+	}
+	ArrayList<CSG> linkParts(DHParameterKinematics d, int linkIndex){
+		ArrayList<CSG> allCad=new ArrayList<>();
+		ArrayList<DHLink> dhLinks = d.getChain().getLinks()
+		DHLink dh = dhLinks.get(linkIndex)
+		// Hardware to engineering units configuration
+		LinkConfiguration conf = d.getLinkConfiguration(linkIndex);
+		// Engineering units to kinematics link (limits and hardware type abstraction)
+		AbstractLink abstractLink = d.getAbstractLink(linkIndex);// Transform used by the UI to render the location of the object
+		// Transform used by the UI to render the location of the object
+		Affine manipulator = dh.getListener();
+		Affine lastLinkFrame
+		def massKg = conf.getMassKg()
+		def centerOfMass = TransformFactory.nrToCSG(conf.getCenterOfMassFromCentroid() )
+		def CMvis = new Sphere(500*massKg).toCSG()
+					.transformed(centerOfMass)
+		
+		if(linkIndex==0)
+			lastLinkFrame=d.getRootListener()
+		else
+			lastLinkFrame=dhLinks.get(linkIndex-1).getListener();
+		
+		def rVal = new Cube(dh.getR()>0?dh.getR():5,1,1).toCSG()
+					.toXMax()
+					.toZMax()
+		rVal.setColor(javafx.scene.paint.Color.RED)
+		CSG profile = new Cube(1,// x dimention
+					20,// y dimention
+				
+				1//  Z dimention
+				)
+				.toCSG()// converts it to a CSG tor display
+				.toYMin()
+				.toZMin()
+				//.toXMin()
+		CSG alpha;
+		double alphaVal = Math.toDegrees(dh.getAlpha())
+		if(Math.abs(alphaVal)>10){
+			alpha= CSG.unionAll(
+			Extrude.revolve(profile,
+						0, // rotation center radius, if 0 it is a circle, larger is a donut. Note it can be negative too
+						Math.abs(alphaVal),// degrees through wich it should sweep
+						(int)10)//number of sweep increments
+			)
+			//.rotz(alphaVal<0?-alphaVal:0)
+		}else{
+			alpha = profile
+		}
+		alpha= alpha.roty(90)
+		if(alphaVal>0){
+			alpha= alpha.rotx(alphaVal)	
+		}
+		alpha= alpha
+			.rotx(-90)
+			.movex(-dh.getR())
+		alpha.setColor(javafx.scene.paint.Color.YELLOW)
+
+		//println name
+		
+
 		def parts = [rVal,alpha,CMvis] as ArrayList<CSG>
+		
+		return parts;
+		
+	}
+	@Override
+	public ArrayList<CSG> generateCad(DHParameterKinematics d, int linkIndex) {
+		
+		ArrayList<DHLink> dhLinks = d.getChain().getLinks()
+		DHLink dh = dhLinks.get(linkIndex)
+		Affine manipulator = dh.getListener();
+		
+		def lastFrameParts = linkLimitParts( d,  linkIndex+1)
+		
+		def parts = linkParts( d,  linkIndex)
+		parts.addAll(lastFrameParts)
 		for(int i=0;i<parts.size();i++){
 			parts.get(i).setManipulator(manipulator);
 			//parts.get(i).setColor(javafx.scene.paint.Color.RED)
 		}
-		for(int i=0;i<lastFrameParts.size();i++){
-			lastFrameParts.get(i).setManipulator(lastLinkFrame);
-			//parts.get(i).setColor(javafx.scene.paint.Color.RED)
-		}
-		parts.addAll(lastFrameParts)
 		return parts;
 
 	}
 
-
+	/**
+	 * Gets the all dh chains.
+	 *
+	 * @return the all dh chains
+	 */
+	public ArrayList<DHParameterKinematics> getLimbDHChains(MobileBase base) {
+		ArrayList<DHParameterKinematics> copy = new ArrayList<DHParameterKinematics>();
+		for(DHParameterKinematics l:base.getLegs()){
+			copy.add(l);	
+		}
+		for(DHParameterKinematics l:base.getAppendages() ){
+			copy.add(l);	
+		}
+		return copy;
+	}
 
 @Override
 	public ArrayList<CSG> generateBody(MobileBase b ) {
@@ -180,7 +234,19 @@ return new ICadGenerator(){
 
 		body.setManipulator(b.getRootListener());
 		body.setColor(javafx.scene.paint.Color.GREY)
-		def parts = [body,IMU, CMvis] as ArrayList<CSG>
+		def parts = [
+		body,
+		IMU, CMvis] as ArrayList<CSG>
+
+		for(DHParameterKinematics l:getLimbDHChains(b)){
+			TransformNR position = l.getRobotToFiducialTransform();
+			Transform csgTrans = TransformFactory.nrToCSG(position)
+			
+			parts.addAll(linkLimitParts( l,  0).collect{
+									it.transformed(csgTrans)
+					}
+				);			
+		}
 		for(int i=0;i<parts.size();i++){
 			parts.get(i)
 			.setManipulator(b.getRootListener());
